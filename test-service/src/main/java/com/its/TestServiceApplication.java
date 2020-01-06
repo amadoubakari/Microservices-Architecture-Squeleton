710eb8e6a957594939c81e131e98808e96ef939c
AMADOU BAKARI <amadoubakari1992@gmail.com>
2020-01-06 05:26:26 -0800

    Intégration of discovery and register server

--- test-service/src/main/java/com/its/TestServiceApplication.java
+++ test-service/src/main/java/com/its/TestServiceApplication.java
@@ -2,12 +2,14 @@ package com.its;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class DemoApplication {
+public class TestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TestServiceApplication.class, args);
 	}
 
 }
